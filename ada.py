c196268e7c6b748862105c2d8c96cd1683b7e29e
franlu0 <162721582+franlu0@users.noreply.github.com>
2024-03-23 19:44:19 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,10 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    a=first_name 
+    b=last_name
+    print(f"{a} {b}".lower())
+    print(f"{a} {b}".title())
+    print(f"{a} {b}".upper())
+    print(f"    {a} {b}".lower())
+ada()
